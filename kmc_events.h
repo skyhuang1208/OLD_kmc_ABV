bca7811cdc0b76858107f0344919d13e29da7bef
Sky <skyhuang@ucla.edu>
2015-02-03 23:59:28 -0800

    v37: further reduce size of history_sol 	history_sol: record change, once per 1000 steps 	history_vcc: record all ltcp, once per 1000 steps

--- kmc_events.h
+++ kmc_events.h
@@ -10,10 +10,8 @@ class class_events{
 	public:
 		class_events(int nx_, int ny_, int nz_, int *nA_, int *nB_, int *nV_, int *nI_, int *states_, 
 			     int n1nbr_, int v1nbr_[][3], int n2nbr_, int v2nbr_[][3], const char name_sol[], const char name_vcc[], bool isrestart):
-		nx(nx_), ny(ny_), nz(nz_), 
-		nA(nA_), nB(nB_), nV(nV_), nI(nI_),
-		states(states_),
-		n1nbr(n1nbr_), v1nbr(v1nbr_), n2nbr(n2nbr_), v2nbr(v2nbr_)
+		nx(nx_), ny(ny_), nz(nz_), nA(nA_), nB(nB_), nV(nV_), nI(nI_),
+		states(states_), n1nbr(n1nbr_), v1nbr(v1nbr_), n2nbr(n2nbr_), v2nbr(v2nbr_)
 		{
 			init_list_vcc();
 
@@ -27,6 +25,9 @@ class class_events{
 			}
 			if(NULL==his_sol) error(2, "(class_events) the solute  history file was not opened!");
 			if(NULL==his_vcc) error(2, "(class_events) the vacancy history file was not opened!");
+	
+			actions_sol[0].reserve(1000); 
+			actions_sol[1].reserve(1000);
 		}
 		
 		// functions
@@ -43,7 +44,9 @@ class class_events{
 		int* const states;	// Don't change the address of this!
 		vector <int> list_vcc;	// A list contains indexs of all vacancies
 		vector <int> list_int;	// A list contains indexs of all interstitials
-
+		
+		vector <int> actions_sol[2]; // A list contains solute atom moves from [0] to [1]
+		
 		int n1nbr, n2nbr;	// number of neighbors
 		int (*v1nbr)[3];	// indexes vectors of 1st neighbors
 		int (*v2nbr)[3];	// indexes vectors of 2nd neighbors
@@ -56,7 +59,7 @@ class class_events{
 		bool is_e2nbr;
 
 		//////functions for all events//////
-		void write_hissol(int timestep, int ltcp_from, int ltcp_to);
+		void write_hissol(int timestep, double totaltime, const vector<int> (&actions_sol)[2]);
 		void write_hisvcc(int timestep, double totaltime);
 		double cal_energy(int x1, int y1, int z1, int x2, int y2, int z2); 
 		void init_list_vcc();
